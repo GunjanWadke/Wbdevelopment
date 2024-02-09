bba938a7f7bc1732180728817a6168fec781cd24
GunjanWadke <159524606+GunjanWadke@users.noreply.github.com>
2024-02-09 14:34:41 +0530

    Delete portfolio directory

--- portfolio/main.js
+++
@@ -1,9 +0,0 @@
-// do {
-//     var typed= new typed(".text", {
-//         String:["frontend Developer", "Web Developer"],
-//         typeSpeed:100,
-//         backSpeed:100,
-//         backDelay:1000,
-//         loop:true
-//     })
-// } while (condition);
